8c20fc44a5ba3bd5b9113bb0eddbdf79a45d51dc
Fazil Chengapra <muhammedfazilchengapra@gmail.com>
2026-05-06 08:45:55 +0530

    .

--- sellix_backend/sellix_backend/settings.py
+++ sellix_backend/sellix_backend/settings.py
@@ -99,13 +99,14 @@ DATABASES = {
 }
 
 # smtp settings
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "shoppeemailer@gmail.com"
-EMAIL_HOST_PASSWORD = "menp qjoh yawl hcim"  # App Password, not Gmail password
-DEFAULT_FROM_EMAIL = "shoppeemailer@gmail.com"
+EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", True)
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:5173")
 
 
 # Password validation
